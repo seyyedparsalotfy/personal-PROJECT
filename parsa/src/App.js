19122f89293484d50a094f7e0d563811d1e86f5d
project <139109816+seyyedparsalotfy@users.noreply.github.com>
2023-11-22 13:05:11 +0000

    Update 5 files

--- parsa/src/App.js
+++ parsa/src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { TypeAnimation } from 'react-type-animation';
+import React from 'react';
+import './App.css' ;
+import 'animate.css';
 
-function App() {
+
+var audio = document.getElementById("myAudio");
+var clickCount = 0;
+
+document.addEventListener("click", function() {
+  clickCount++;
+
+  if (clickCount === 1) {
+    audio.play();
+    setTimeout(function() {
+      clickCount = 0;
+    }, 4000);
+  }
+});
+
+export const MMa =() =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    <div  class="M">
+    <div class="animate__animated animate__slideInLeft">  
+    <TypeAnimation
+    class="M"
+      style={{   
+      whiteSpace: 'pre-line',  
+      fontFamily :'Rockwell' ,
+     
+  
+      
+    }}
+   sequence={[
+    'My skills:\n Html \n Css \n Java script \n es6 to es10 \n React \n Developer enviroment \n rest full Api \n Boot strap \n css Grid \n design responsive \n Git.Github',
+    9000,
+    '',
+
+   
+  ]}
+  speed={{type: 'keyStrokeDelayInMs' , value : 150}}
+  repeat={Infinity}
+    />
+    </div>
     </div>
   );
-}
+};
+export const ExampleComponent = () => {
+ 
+  return (
+ 
+    <div  class="text" >
+      <div class="animate__animated animate__slideInLeft" >
+    <TypeAnimation
+   sequence={[
+    'Hello',
+    1000,
+    'im seyyed parsa lotfy',
+    1000,
+    'im seyyed parsa lotfy im developer.',
+    1000,
+    'im seyyed parsa lotfy im web designer.',
+    2000,
+    'im seyyed parsa lotfy im full stack developer.',
+    2000,
+  ]}
+  speed={10}
+  style={{   fontSize: '200%' ,
+ 
 
-export default App;
+ }}
+  repeat={Infinity}
+    />
+    </div>
+ </div>  
+  );
+};
+
+
+
+
