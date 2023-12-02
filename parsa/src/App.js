e0aabc8bf0b0b112be52c75789c45a5ee655e228
project <139109816+seyyedparsalotfy@users.noreply.github.com>
2023-12-02 11:16:01 +0000

    Update index.html and App.js

--- parsa/src/App.js
+++ parsa/src/App.js
@@ -1,82 +1,66 @@
-import { TypeAnimation } from 'react-type-animation';
-import React from 'react';
-import './App.css' ;
-import 'animate.css';
-
+import { TypeAnimation } from "react-type-animation";
+import React from "react";
+import "./App.css";
+import "animate.css";
 
 var audio = document.getElementById("myAudio");
 var clickCount = 0;
 
-document.addEventListener("click", function() {
+document.addEventListener("click", function () {
   clickCount++;
 
   if (clickCount === 1) {
     audio.play();
-    setTimeout(function() {
+    setTimeout(function () {
       clickCount = 0;
     }, 4000);
   }
 });
 
-export const MMa =() =>{
+export const MMa = () => {
   return (
-  
-    <div  class="M">
-    <div class="animate__animated animate__slideInLeft">  
-    <TypeAnimation
-    class="M"
-      style={{   
-      whiteSpace: 'pre-line',  
-      fontFamily :'Rockwell' ,
-     
-  
-      
-    }}
-   sequence={[
-    'My skills:\n Html \n Css \n Java script \n es6 to es10 \n React \n Developer enviroment \n rest full Api \n Boot strap \n css Grid \n design responsive \n Git.Github',
-    9000,
-    '',
-
-   
-  ]}
-  speed={{type: 'keyStrokeDelayInMs' , value : 150}}
-  repeat={Infinity}
-    />
-    </div>
+    <div class="M">
+      <div class="animate__animated animate__slideInLeft">
+        <TypeAnimation
+          class="M"
+          style={{
+            whiteSpace: "pre-line",
+            fontFamily: "Rockwell",
+          }}
+          sequence={[
+            "My skills:\n Html \n Css \n Java script \n es6 to es10 \n React \n Developer enviroment \n rest full Api \n Boot strap \n css Grid \n design responsive \n Git.Github",
+            9000,
+            "",
+          ]}
+          speed={{ type: "keyStrokeDelayInMs", value: 150 }}
+          repeat={Infinity}
+        />
+      </div>
     </div>
   );
 };
 export const ExampleComponent = () => {
- 
   return (
- 
-    <div  class="text" >
-      <div class="animate__animated animate__slideInLeft" >
-    <TypeAnimation
-   sequence={[
-    'Hello',
-    1000,
-    'im seyyed parsa lotfy',
-    1000,
-    'im seyyed parsa lotfy im developer.',
-    1000,
-    'im seyyed parsa lotfy im web designer.',
-    2000,
-    'im seyyed parsa lotfy im full stack developer.',
-    2000,
-  ]}
-  speed={10}
-  style={{   fontSize: '200%' ,
- 
-
- }}
-  repeat={Infinity}
-    />
+    <div class="text">
+      <div class="animate__animated animate__slideInLeft">
+        <TypeAnimation
+          sequence={[
+            "Hello",
+            1000,
+            "im seyyed parsa lotfy",
+            1000,
+            "im seyyed parsa lotfy im developer.",
+            1000,
+            "im seyyed parsa lotfy im web designer.",
+            2000,
+            "im seyyed parsa lotfy im full stack developer.",
+            2000,
+          ]}
+          speed={10}
+          style={{ fontSize: "200%" }}
+          repeat={Infinity}
+        />
+      </div>
     </div>
- </div>  
   );
 };
-
-
-
-
