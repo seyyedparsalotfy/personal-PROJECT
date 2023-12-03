173ba25c9877cbc77642d33e17b5712da52ddaf9
project <139109816+seyyedparsalotfy@users.noreply.github.com>
2023-12-03 12:46:14 +0000

    Update index.html and index.js

--- parsa/src/index.js
+++ parsa/src/index.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { MMa , ExampleComponent}from './App';
-import reportWebVitals from './reportWebVitals';
-import 'tachyons' ;
-import 'animate.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { MMa, ExampleComponent } from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "tachyons";
+import "animate.css";
 
 ReactDOM.render(
-    <div>
-                    <MMa/>
-                <ExampleComponent/>
-
-               
-</div>
-,document.getElementById('root')
+  <div>
+    <MMa />
+    <ExampleComponent />
+  </div>,
+  document.getElementById("root"),
 );
 
 reportWebVitals();
